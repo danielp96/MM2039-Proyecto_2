61f405304ecd594b5ee342ee8a5f7abc04d2c1be
Daniel Pineda <daniel-p96@outlook.com>
2021-11-30 18:07:12 -0600

    New data, fixed script.

--- script.m
+++ script.m
@@ -1,12 +1,8 @@
 %
 
 % data1 = voltaje referencia, data2 = voltaje capacitor
-[t, data1, data2] = filter_data('./data.csv', 40, 5/125, 200, 400);
-
-% remover plot luego
-plot(data1);
-hold on
-plot(data2);
+[t1, clk1, data1] = filter_data('./data_1uF.csv', 40, 5/125, 40, 253);
+[t2, clk2, data2] = filter_data('./data_10uF.csv', 40, 5/125, 428, 670);
 
 % Global
 global R1 R2 R3 C1 C2 V qK q2
@@ -17,27 +13,31 @@ R2 = 330;
 R3 = 330;
 C1 = 1e-6;
 C2 = 10e-6; 
-V = 10.00; %Voltage Source
+V = 5.00; %Voltage Source
 qK = C1*(R3*(R1+R2)+R1*R2); %F1 Den Constant
-q2 = C2*R1*R3; % F2 Den Constant
-h = 0.1;
+q2 = C2*R3; % F2 Den Constant
+h = 0.00001;
 V1 = 0;
 V2 = 0;
 V1_f = [V1];
 V2_f = [V2];
-l = linspace(0,0.5,1000);
+l = linspace(0,0.5,5000);
 
 
- for i = 1:999
-  [K11] = f1_get(V1_f(i), V2_f(i));
-  [K12] = f2_get(V1_f(i), V2_f(i));
-  [K21] = f1_get((V1_f(i)+ (3/4)*h*K11),(V2_f(i) + (3/4)*h*K12));
-  [K22] = f2_get((V1_f(i)+ (3/4)*h*K11),(V2_f(i) + (3/4)*h*K12));
-  m = V1_f(i)+ ((1/3)*K11 + (2/3)*K21)*h;
-  V1_f = [V1_f m];
-  n = V2_f(i)+  ((1/3)*K12 + (2/3)*K22)*h;
-  V2_f = [V2_f n];
- end
+for i = 1:size(l, 2)
+    [K11] = f1_get(V1_f(i), V2_f(i));
+    [K12] = f2_get(V1_f(i), V2_f(i), K11);
+
+    [K21] = f1_get((V1_f(i)+ 0.5*h*K11),(V2_f(i) + 0.5*h*K12));
+    [K22] = f2_get((V1_f(i)+ 0.5*h*K11),(V2_f(i) + 0.5*h*K12), K21);
+
+    m = V1_f(i)+ (0.5*K11 + 0.5*K21)*h;
+    V1_f = [V1_f m];
+    
+    n = V2_f(i)+ (0.5*K12 + 0.5*K22)*h;
+    V2_f = [V2_f n];
+end
+
 
 function [t, data1, data2] = filter_data(file, offset, scale, start_x, end_x)
     data_temp = readtable(file);
@@ -57,7 +57,7 @@ function [f1] = f1_get(V1, V2)
     f1 = (R3*V+ V2*R1 - V1*(R1+R3))/(qK);
 end
 
-function [f2] = f2_get(V1, V2)
-global  R2  C1  q2 
-f2 = (V1-V2+C1*R2*f1_get(V1,V2))/(q2);
+function [f2] = f2_get(V1, V2, dV1)
+    global  R2  C1  q2 
+    f2 = (V1-V2+C1*R2*dV1)/(q2);
 end
